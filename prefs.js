3bf3bf60c1c5975f13f3d9b884f8ef3fcb03952d
Simon Kornblith <simon@simonster.com>
2010-12-21 07:10:58 +0000

    allow scripts in connector.js to go on as long as they want

--- prefs.js
+++ prefs.js
@@ -1,3 +1,4 @@
 pref("javascript.options.showInConsole", true);
 pref("toolkit.defaultChromeURI", "chrome://zotero/content/standalone.xul");
 pref("network.prefetch-next", false);
+pref("dom.max_chrome_script_run_time", 0);
