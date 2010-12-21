11ef8d2435c010c909defca214c7e49580fa7bd7
Simon Kornblith <simon@simonster.com>
2010-12-21 19:25:14 +0000

    disable out-of-process java, since we can

--- prefs.js
+++ prefs.js
@@ -2,3 +2,5 @@ pref("javascript.options.showInConsole", true);
 pref("toolkit.defaultChromeURI", "chrome://zotero/content/standalone.xul");
 pref("network.prefetch-next", false);
 pref("dom.max_chrome_script_run_time", 0);
+pref("dom.ipc.plugins.java.enabled", false);
+pref("javascript.options.methodjit.chrome", true);
