27ac76f66d264d5cabd9b54145d0c98910ccb54d
Simon Kornblith <simon@simonster.com>
2011-08-16 19:05:01 +0000

    Use XULRunner 6 final (although this may not differ from the previous build at all) and mark as compatible with XULRunner 7

--- fetch_xulrunner.sh
+++ fetch_xulrunner.sh
@@ -22,7 +22,7 @@ GET_MAC=1
 GET_WIN32=1
 GET_LINUX=1
 
-SITE=https://ftp.mozilla.org/pub/mozilla.org/xulrunner/nightly/2011-08-05-03-07-53-mozilla-beta/
+SITE=https://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/6.0/runtimes/
 VERSION=6.0
 
 rm -rf xulrunner
