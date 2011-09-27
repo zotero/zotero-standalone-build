369490d645e616c2860e9b76f49c44516cd5f156
Simon Kornblith <simon@simonster.com>
2011-09-27 11:31:37 -0400

    Tweak auto-update builds

--- update-packaging/build_autoupdate.sh
+++ update-packaging/build_autoupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FROM=2.1a3
-TO=3.0b1
+FROM=3.0b2
+TO=3.0b2.1
 USE_LOCAL_TO=1
 CALLDIR=`pwd`
 DISTDIR=$CALLDIR/../dist
@@ -23,26 +23,31 @@ for version in "$FROM" "$TO"; do
 	cd $versiondir
 	
 	# Download archives
-	for archive in "Zotero.dmg" "Zotero_win32.zip" "Zotero_linux-i686.tar.bz2" "Zotero_linux-x86_64.tar.bz2"; do
+	MAC_ARCHIVE="Zotero-${version}.dmg"
+	WIN_ARCHIVE="Zotero-${version}_win32.zip"
+	LINUX_X86_ARCHIVE="Zotero-${version}_linux-i686.tar.bz2"
+	LINUX_X86_64_ARCHIVE="Zotero-${version}_linux-x86_64.tar.bz2"
+	
+	for archive in "$MAC_ARCHIVE" "$WIN_ARCHIVE" "$LINUX_X86_ARCHIVE" "$LINUX_X86_64_ARCHIVE"; do
 		rm -f $archive
-		wget -nv http://www.zotero.org/download/standalone/$version/$archive
+		wget -nv http://download.zotero.org/standalone/$version/$archive
 	done
 	
 	# Unpack Zotero.app
 	hdiutil detach -quiet /Volumes/Zotero 2>/dev/null
-	hdiutil attach -quiet Zotero.dmg
+	hdiutil attach -quiet "$MAC_ARCHIVE"
 	cp -R /Volumes/Zotero/Zotero.app $versiondir
-	rm Zotero.dmg
+	rm "$MAC_ARCHIVE"
 	hdiutil detach -quiet /Volumes/Zotero
 	
 	# Unpack Win32 zip
-	unzip -q Zotero_win32.zip
-	rm Zotero_win32.zip
+	unzip -q "$WIN_ARCHIVE"
+	rm "$WIN_ARCHIVE"
 	
 	# Unpack Linux tarballs
-	for build in "Zotero_linux-i686" "Zotero_linux-x86_64"; do
-		tar -xjf $build.tar.bz2
-		rm $build.tar.bz2
+	for build in "$LINUX_X86_ARCHIVE" "$LINUX_X86_64_ARCHIVE"; do
+		tar -xjf "$build"
+		rm "$build"
 	done
 done
 
@@ -55,3 +60,7 @@ for build in "mac" "win32" "linux-i686" "linux-x86_64"; do
 	$CALLDIR/make_incremental_update.sh $DISTDIR/Zotero-${TO}-${FROM}_$build.mar $STAGEDIR/$FROM/$dir $STAGEDIR/$TO/$dir
 	$CALLDIR/make_full_update.sh $DISTDIR/Zotero-${TO}-full_$build.mar $STAGEDIR/$TO/$dir
 done
+
+cd "$DISTDIR"
+shasum -a 512 * > sha512sums
+ls -la > files
