cbe72146aa731f07a749a4cddc3582df04837009
Simon Kornblith <simon@simonster.com>
2012-07-03 00:51:05 -0400

    Move signing configuration to config.sh

--- config.sh
+++ config.sh
@@ -12,13 +12,19 @@ WIN32_RUNTIME_PATH="`pwd`/xulrunner/xulrunner_win32"
 LINUX_i686_RUNTIME_PATH="`pwd`/xulrunner/xulrunner_linux-i686"
 LINUX_x86_64_RUNTIME_PATH="`pwd`/xulrunner/xulrunner_linux-x86_64"
 
+# Whether to sign builds
+SIGN=1
+
+# OS X Developer ID certificate information
+DEVELOPER_ID=c8a15a3bc9eaaabc112e83b2f885609e535d07f0
+CODESIGN_REQUIREMENTS="=designated => anchor apple generic  and identifier \"org.zotero.zotero\" and ((cert leaf[field.1.2.840.113635.100.6.1.9] exists) or ( certificate 1[field.1.2.840.113635.100.6.2.6] exists and certificate leaf[field.1.2.840.113635.100.6.1.13] exists  and certificate leaf[subject.OU] = \"8LAYR367YV\" ))"
+
 # Paths for Windows installer build
 MAKENSISU='C:\Program Files (x86)\NSIS\Unicode\makensis.exe'
 UPX='C:\Program Files (x86)\upx\upx.exe'
 EXE7ZIP='C:\Program Files\7-Zip\7z.exe'
 
 # Paths for Windows installer build only necessary for signed binaries
-SIGN=1
 SIGNTOOL='C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\signtool.exe'
 SIGNATURE_URL='https://www.zotero.org/'
 
