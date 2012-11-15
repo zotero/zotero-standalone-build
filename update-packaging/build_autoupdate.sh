4f47066b12959864c203121a70a0f2a915239ae6
Simon Kornblith <simon@simonster.com>
2012-11-15 17:06:33 -0500

    Remove obsolete files

--- update-packaging/build_autoupdate.sh
+++ update-packaging/build_autoupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FROM=3.0b2
-TO=3.0b2.1
+FROM=3.0.8
+TO=3.0.9
 USE_LOCAL_TO=1
 CALLDIR=`pwd`
 DISTDIR=$CALLDIR/../dist
@@ -57,8 +57,10 @@ for build in "mac" "win32" "linux-i686" "linux-x86_64"; do
 	else
 		dir="Zotero_$build"
 	fi
-	$CALLDIR/make_incremental_update.sh $DISTDIR/Zotero-${TO}-${FROM}_$build.mar $STAGEDIR/$FROM/$dir $STAGEDIR/$TO/$dir
-	$CALLDIR/make_full_update.sh $DISTDIR/Zotero-${TO}-full_$build.mar $STAGEDIR/$TO/$dir
+	cp "$CALLDIR/removed-files" "$STAGEDIR/$TO/$dir"
+	touch "$STAGEDIR/$TO/$dir/precomplete"
+	"$CALLDIR/make_incremental_update.sh" "$DISTDIR/Zotero-${TO}-${FROM}_$build.mar" "$STAGEDIR/$FROM/$dir" "$STAGEDIR/$TO/$dir"
+	"$CALLDIR/make_full_update.sh" "$DISTDIR/Zotero-${TO}-full_$build.mar" "$STAGEDIR/$TO/$dir"
 done
 
 cd "$DISTDIR"
