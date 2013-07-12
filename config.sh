d12d42d8f6e2abbe0cf8dc4845ab079e20ea2490
Simon Kornblith <simon@simonster.com>
2013-07-11 23:28:14 -0400

    Update Gecko version

--- config.sh
+++ config.sh
@@ -4,8 +4,8 @@ BUILD_WIN32=1
 BUILD_LINUX=1
 
 # Version of Gecko to build with
-GECKO_VERSION="20.0"
-GECKO_SHORT_VERSION="20.0"
+GECKO_VERSION="22.0"
+GECKO_SHORT_VERSION="22.0"
 
 # Paths to Gecko runtimes
 MAC_RUNTIME_PATH="`pwd`/xulrunner/XUL.framework"
