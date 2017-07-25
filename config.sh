48fe64672f29582ae174783a0bb985114f0d9795
Dan Stillman <dstillman@zotero.org>
2017-07-25 16:42:59 -0400

    Update Windows to 52.2.1esr

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # xulrunner-stub.exe currently requires <=47, though it can probably be rebuilt against a later SDK
 GECKO_VERSION_MAC="54.0"
 GECKO_VERSION_LINUX="54.0"
-GECKO_VERSION_WIN="52.2.0esr"
+GECKO_VERSION_WIN="52.2.1esr"
 
 # Paths to Gecko runtimes
 MAC_RUNTIME_PATH="$DIR/xulrunner/Firefox.app"
