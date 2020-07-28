f21a31da73a0dee3639dca87185ad390da9da942
Dan Stillman <dstillman@zotero.org>
2020-07-28 13:31:57 -0400

    Update PDF tools

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ WIN32_RUNTIME_PATH="$DIR/xulrunner/firefox-win32"
 LINUX_i686_RUNTIME_PATH="$DIR/xulrunner/firefox-i686"
 LINUX_x86_64_RUNTIME_PATH="$DIR/xulrunner/firefox-x86_64"
 
-PDF_TOOLS_VERSION="0.0.3"
+PDF_TOOLS_VERSION="0.0.4"
 PDF_TOOLS_URL="https://zotero-download.s3.amazonaws.com/pdftools/pdftools-$PDF_TOOLS_VERSION.tar.gz"
 
 # Whether to sign builds
