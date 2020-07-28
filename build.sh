a86a503f97d1a5116ff14ee009f9de28cc7e624a
Dan Stillman <dstillman@zotero.org>
2020-07-28 12:42:30 -0400

    Update path to custom MacOS components

--- build.sh
+++ build.sh
@@ -245,8 +245,8 @@ if [ $BUILD_MAC == 1 ]; then
 	cp "$CALLDIR/mac/zotero" "$CONTENTSDIR/MacOS/zotero"
 	cp "$BUILD_DIR/application.ini" "$CONTENTSDIR/Resources"
 
-	# TEMP: Modified versions of some Firefox components for Big Sur, placed in xulrunner/Fx60-MacOS
-	cp "$MAC_RUNTIME_PATH/../Fx60-MacOS/"{libnss3.dylib,XUL} "$CONTENTSDIR/MacOS/"
+	# TEMP: Modified versions of some Firefox components for Big Sur, placed in xulrunner/MacOS
+	cp "$MAC_RUNTIME_PATH/../MacOS/"{libnss3.dylib,XUL} "$CONTENTSDIR/MacOS/"
 
 	# Use our own updater, because Mozilla's requires updates signed by Mozilla
 	cd "$CONTENTSDIR/MacOS"
