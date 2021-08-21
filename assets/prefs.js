e27d32c879879a5557a59010a14f220a81bef589
J. Ryan Stinnett <jryans@gmail.com>
2021-08-22 00:42:04 +0100

    fx-compat: Remove unused error console pref

--- assets/prefs.js
+++ assets/prefs.js
@@ -176,8 +176,5 @@ pref("app.update.channel", "default");
 // This should probably not be a preference that's used in toolkit....
 pref("browser.preferences.instantApply", false);
 
-// Whether to show the Error Console option in the Tools menu
-pref("devtools.errorconsole.enabled", false);
-
 // Allow elements to be displayed full-screen
 pref("full-screen-api.enabled", true);
