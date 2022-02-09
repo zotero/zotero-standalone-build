ad8ecc8761c0950db2a79f9a52e39c8a21bf8da6
Dan Stillman <dstillman@zotero.org>
2022-02-09 05:53:58 -0500

    Add LLVM 6 libc++.1.dylib to custom binaries on macOS
    Previously, our XUL accidentally linked to
    /usr/local/opt/llvm@6/lib/libc++.1.dylib from our build machine, and
    that somehow worked for everyone in earlier versions of macOS. In macOS
    12.3 beta 2, it no longer does:
    
    https://forums.zotero.org/discussion/94408/zotero-does-not-run-under-macos-12-3beta
    
    Unclear what changed, but to fix, we bundle the LLVM 6 libc++.1.dylib
    and update XUL to point to @executable_path/libc++.1.dylib instead of an
    absolute path using this magic command:
    
    `install_name_tool -change /usr/local/opt/llvm@6/lib/libc++.1.dylib @executable_path/libc++.1.dylib XUL`
    
    The custom components are stored in S3 and downloaded from
    `custom_components_url` by our build script.

--- build.sh
+++ build.sh
@@ -260,7 +260,7 @@ if [ $BUILD_MAC == 1 ]; then
 	cp "$BUILD_DIR/application.ini" "$CONTENTSDIR/Resources"
 
 	# TEMP: Modified versions of some Firefox components for Big Sur, placed in xulrunner/MacOS
-	cp "$MAC_RUNTIME_PATH/../MacOS/"{libnss3.dylib,XUL} "$CONTENTSDIR/MacOS/"
+	cp "$MAC_RUNTIME_PATH/../MacOS/"{libc++.1.dylib,libnss3.dylib,XUL} "$CONTENTSDIR/MacOS/"
 
 	# Use our own updater, because Mozilla's requires updates signed by Mozilla
 	cd "$CONTENTSDIR/MacOS"
