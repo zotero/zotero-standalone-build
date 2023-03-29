9eeb2ae575509ee73ff413f956f08dd3ca1dd8cb
Dan Stillman <dstillman@zotero.org>
2023-03-29 04:16:25 -0400

    Clarify comment for removed-files handling on macOS

--- build.sh
+++ build.sh
@@ -456,8 +456,8 @@ if [ $BUILD_MAC == 1 ]; then
 	find "$CONTENTSDIR" -depth -type d -name .git -exec rm -rf {} \;
 	find "$CONTENTSDIR" \( -name .DS_Store -or -name update.rdf \) -exec rm -f {} \;
 
-	# Copy over removed-files and make a precomplete file since it
-	# needs to be stable for the signature
+	# Copy over removed-files and make a precomplete file here since it needs to be stable for the
+	# signature. This is done in build_autocomplete.sh for other platforms.
 	cp "$CALLDIR/update-packaging/removed-files_mac" "$CONTENTSDIR/Resources/removed-files"
 	touch "$CONTENTSDIR/Resources/precomplete"
 	
